d852b41e2d71db721b67cac55fed16b7cdcf99aa
Renato Jiquiriçá Ramos <rjramos70@Renatos-MacBook-Pro.local>
2018-06-29 19:33:24 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/havefun/api/HaveFunApplication.java
@@ -0,0 +1,12 @@
+package com.havefun.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaveFunApplication.class, args);
+	}
+}
